9df2c434a7382e7af24b613a9492c16a9f592c7c
Turing99 <andrei.fratila99@yahoo.ro>
2022-02-08 23:50:26 +0200

    Add project files.

---
+++ RandomPasswordGenerator/Generator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace RandomPasswordGenerator
+{
+    public class Generator
+    {
+        private static readonly char[] chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!#$%'()*+`-./".ToCharArray();
+        public static string Generate(int size)
+        {
+            byte[] data = new byte[size * 10];
+            using (RNGCryptoServiceProvider cryptoProvider = new RNGCryptoServiceProvider())
+                cryptoProvider.GetBytes(data);
+
+            StringBuilder builder = new StringBuilder(size);
+
+            for (int i = 0; i < size; i++)
+            {
+                long random = BitConverter.ToUInt32(data, i * 10) % chars.Length;
+                builder.Append(chars[random]);
+            }
+            return builder.ToString();
+        }
+    }
+}
